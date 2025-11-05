1f588da77d8d22ec5d5bbe91f31054167854568f
inseong01 <insung940@naver.com>
2025-11-05 16:42:09 +0900

    feat: 검증기 메서드 세분화
    - 사용자 입력 검증으로 수정
    - 커스텀, 기본 구분자 로직 메서드 구분

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -5,18 +5,55 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validator {
-  public static void validateSeperator(String seperator) {
-    if (seperator.isEmpty()) {
-      throw new IllegalArgumentException("[ERROR] 구분자를 입력해주세요.");
+  public void validateUserInput(String input) {
+    if (!this.isStartCustom(input)) {
+      this.validateBaseInput(input);
+      return;
     }
+
+    this.validateCustomInput(input);
+  }
+
+  public void validateBaseInput(String input) {
+    Pattern stringPattern = Pattern.compile("\\D");
+
+    Matcher stringMatcher = stringPattern.matcher(input);
+
+    if (!stringMatcher.find()) return;
+
+    Pattern baseSeparatorPattern = Pattern.compile("^[,;]");
+
+    Matcher baseSeparatorMatcher = baseSeparatorPattern.matcher(input);
+    if (!baseSeparatorMatcher.find()) {
+      throw new IllegalArgumentException("[ERROR] 기본 구분자로 설정되지 않았습니다.");
+    }
+  }
+
+  public void validateCustomInput(String input) {
+    Pattern customSeparatorPattern = Pattern.compile("^//(.+)\\\\n");
+
+    Matcher customSeparatorMatcher = customSeparatorPattern.matcher(input);
+
+    if (!customSeparatorMatcher.find()) {
+      throw new IllegalArgumentException("[ERROR] 커스텀 문자가 설정되지 않았습니다.");
+    }
+
+    String customSeparator = customSeparatorMatcher.group(1);
 
     Pattern numberPattern = Pattern.compile("\\d");
 
-    Matcher numberMatcher = numberPattern.matcher(seperator);
-
+    Matcher numberMatcher = numberPattern.matcher(customSeparator);
     if (numberMatcher.find()) {
-      throw new IllegalArgumentException("[ERROR] 구분자는 숫자 외 문자여야 합니다.");
+      throw new IllegalArgumentException("[ERROR] 구분자는 숫자가 아닌 문자여야 합니다.");
     }
+  }
+
+  private Boolean isStartCustom(String input) {
+    Pattern customSeparatorPattern = Pattern.compile("^//.*\\\\n.+");
+
+    Matcher customSeparatorMatcher = customSeparatorPattern.matcher(input);
+
+    return customSeparatorMatcher.find();
   }
 
   public static void validateNumbers(int[] numbers) {
--- src/test/java/calculator/ValidatorTest.java
+++ src/test/java/calculator/ValidatorTest.java
@@ -8,18 +8,29 @@ public class ValidatorTest {
   Validator validator = new Validator();
 
   @Test
-  void 구분자가_있는_경우_통과() {
-    assertThatCode(() -> validator.validateSeperator("!")).doesNotThrowAnyException();
+  void 올바른_문장인_경우_통과() {
+    assertThatCode(() -> validator.validateUserInput("123;24,53")).doesNotThrowAnyException();
+
+    assertThatCode(() -> validator.validateUserInput("")).doesNotThrowAnyException();
+
+    assertThatCode(() -> validator.validateUserInput("//a\\n1a2a3")).doesNotThrowAnyException();
   }
 
   @Test
-  void 구분자가_없는_경우_예외_처리() {
-    assertThatThrownBy(() -> validator.validateSeperator("")).isInstanceOf(IllegalArgumentException.class);
+  void 커스텀_구분자가_없는_경우_예외_처리() {
+    assertThatThrownBy(() -> validator.validateCustomInput("//\\n1a2a3")).isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
-  void 구분자가_숫자인_경우_예외_처리() {
-    assertThatThrownBy(() -> validator.validateSeperator("12")).isInstanceOf(IllegalArgumentException.class);
+  void 커스텀_구분자가_숫자인_경우_예외_처리() {
+    assertThatThrownBy(() -> validator.validateCustomInput("//12\\n312412512")).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void 기본_구분자가_아닌_경우_예외_처리() {
+    assertThatThrownBy(() -> validator.validateBaseInput("1!2@3$4")).isInstanceOf(IllegalArgumentException.class);
+
+    assertThatThrownBy(() -> validator.validateBaseInput(" ")).isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
