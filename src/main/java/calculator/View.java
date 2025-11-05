e517da4b0fc040730c6432f0cacfaa394123bca4
inseong01 <insung940@naver.com>
2025-11-05 14:04:57 +0900

    feat: 입출력 클래스 구현
    - 입출력 메서드, View 클래스로 통합

---
+++ src/main/java/calculator/View.java
@@ -0,0 +1,17 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class View {
+  public static String input() {
+    String input;
+
+    input = Console.readLine();
+
+    return input;
+  }
+
+  public static void output(String result) {
+    System.out.println("결과 : " + result);
+  }
+}
