f1dd99788ef919597105d7b45428c4898c8f0778
inseong01 <insung940@naver.com>
2025-11-06 15:13:02 +0900

    feat: 애플리케이션 클래스 구현
    - 컨트롤러 메서드 흐름 별 배치

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,13 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
   public static void main(String[] args) {
-    // TODO: 프로그램 구현
+    CalculatorController calculatorController = new CalculatorController();
+
+    calculatorController.getUserInput();
+
+    calculatorController.parseUserInput();
+
+    calculatorController.printUserInput();
   }
 }
